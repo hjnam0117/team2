9f7905d4b0076394b7a606d1da1737d0902ef794
남현지 <@192.168.1.143>
2022-06-15 21:01:06 +0900

    2

---
+++ jwbook/src/main/java/bookstore/cart.java
@@ -0,0 +1,54 @@
+package bookstore;
+
+public class cart {
+	private int cartid;
+	private int bookid;//번호
+	private String bookname;//책 이름
+	private String bookwriter;//작가
+	private int bookcount;//주문 수량
+	private int totalprice;//가격
+	private String id;
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getBookwriter() {
+		return bookwriter;
+	}
+	public void setBookwriter(String bookwriter) {
+		this.bookwriter = bookwriter;
+	}
+	public int getBookcount() {
+		return bookcount;
+	}
+	public void setBookcount(int bookcount) {
+		this.bookcount = bookcount;
+	}
+	public int getTotalprice() {
+		return totalprice;
+	}
+	public void setTotalprice(int totalprice) {
+		this.totalprice = totalprice;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getCartid() {
+		return cartid;
+	}
+	public void setCartid(int cartid) {
+		this.cartid = cartid;
+	}
+	
+}
---
+++ jwbook/src/main/java/bookstore/userDAO.java
@@ -0,0 +1,51 @@
+package bookstore;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class userDAO {
+	
+	Connection conn=null;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	final String JDBC_DRIVER="org.h2.Driver";
+	final String JDBC_URL="jdbc:h2:tcp://localhost/~/jwbookdb";
+	
+	public Connection open() {
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn=DriverManager.getConnection(JDBC_URL, "jwbook", "1234");
+		}catch(Exception e) {e.printStackTrace();}
+		return conn;
+	}
+	
+	public void close() {
+		try {
+			pstmt.close();
+			conn.close();
+		}catch(SQLException e) {e.printStackTrace();}
+	}
+	
+	public void insert(userdb u) {
+		Connection conn=open();
+		String sql=
+				"INSERT INTO userdb(id, pw, call, address, email) values(?,?,?,?,?)";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, u.getId());
+			pstmt.setString(2, u.getPw());
+			pstmt.setString(3, u.getCall());
+			pstmt.setString(4, u.getAddress());
+			pstmt.setString(5, u.getEmail());
+			
+			pstmt.executeUpdate();
+		}catch(Exception e) {e.printStackTrace();}
+		finally {close();}
+	}
+}
